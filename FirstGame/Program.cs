f2ddef5bffd7df442b8a958ea705191c9d0b0aff
RuslanSK88 <ruslansk88@gmail.com>
2023-02-15 19:56:27 +0600

    Добавили логику в Program

--- FirstGame/Program.cs
+++ FirstGame/Program.cs
@@ -1,8 +1,39 @@
 ﻿namespace FirstGame;
 class Program
 {
-    public static void Main()
+    private static Game _game = new Game();
+    static void Main()
     {
-        Console.WriteLine("Hello World!");
+
+        while (_game.GetWinner() == Winner.None)
+        {
+            PrintGame();
+            _game.ChooseLetter();
+            _game.CheckUserInput();
+        }
+        if (Winner.Win == _game.GetWinner())
+        {
+            PrintGame();
+            Console.WriteLine();
+            Console.WriteLine("Ты победил");
+        }
+        else
+        {
+            PrintGame();
+            Console.WriteLine();
+            Console.WriteLine("Ты проиграл");
+            Console.WriteLine($"Правильное слово - {_game.RndWord}");
+        }
+
+    }
+    static void PrintGame()
+    {
+
+        Console.Clear();
+        Console.WriteLine("Перед вами игра - Виселица");
+        Console.WriteLine($"Число попыток = {_game.Counter}");
+        Console.WriteLine(_game.HiddenWord);
+        Console.Write("Введите букву: ");
     }
 }
+
